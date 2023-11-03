811f890a7935ccb213547f0368234ab9dab60753
m1racl3 <eralmendral@gmail.com>
2023-11-03 20:43:32 +0800

    folder setup

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button'
+
+export default function Home() {
+    return (
+        <div>
+            <h1 className="font-bold text-rose-500">Hello RNotion</h1>
+            <Button variant="purple"> Click Me </Button>
+        </div>
+    )
+}
